77e1f9aba8b94a09dcfd9f8c8baf7d357e3f7fa5
takagi <iam.takagi.dev@gmail.com>
2022-10-29 22:31:18 +0900

    feat: user.getWeeklyTrackChart を使う

--- src/lastfm.ts
+++ src/lastfm.ts
@@ -3,22 +3,22 @@ import axios from 'axios';
 export const getTopTrackFromLastfm = async () => {
     const client = axios.create()
     const params = new URLSearchParams()
-    params.append("method", "user.gettoptracks")
+    params.append("method", "user.getWeeklyTrackChart")
     params.append("user", process.env.LASTFM_USER_ID ?? "")
     params.append("limit", "1")
     params.append("api_key", process.env.LASTFM_API_KEY ?? "")
     params.append("format", "json")
-    const result = await client.get<ToptracksResnponse.RootObject>('https://ws.audioscrobbler.com/2.0/?' + params.toString()).then(({data}) => {
-        if(!data || !data.toptracks) return undefined
-        return data.toptracks.track[0]
+    const result = await client.get<WeeklytrackchartResponse.RootObject>('https://ws.audioscrobbler.com/2.0/?' + params.toString()).then(({data}) => {
+        if(!data || !data.weeklytrackchart) return undefined
+        return data.weeklytrackchart.track[0]
     })
     return result
 }
 
-declare module ToptracksResnponse {
+declare module WeeklytrackchartResponse {
 
-    export interface Streamable {
-        fulltrack: string;
+    export interface Artist {
+        mbid: string;
         '#text': string;
     }
 
@@ -27,43 +27,32 @@ declare module ToptracksResnponse {
         '#text': string;
     }
 
-    export interface Artist {
-        url: string;
-        name: string;
-        mbid: string;
-    }
-
     export interface Attr {
         rank: string;
     }
 
     export interface Track {
-        streamable: Streamable;
-        mbid: string;
-        name: string;
-        image: Image[];
         artist: Artist;
+        image: Image[];
+        mbid: string;
         url: string;
-        duration: string;
+        name: string;
         '@attr': Attr;
         playcount: string;
     }
 
     export interface Attr2 {
+        from: string;
         user: string;
-        totalPages: string;
-        page: string;
-        perPage: string;
-        total: string;
+        to: string;
     }
 
-    export interface Toptracks {
+    export interface Weeklytrackchart {
         track: Track[];
         '@attr': Attr2;
     }
 
     export interface RootObject {
-        toptracks: Toptracks;
+        weeklytrackchart: Weeklytrackchart;
     }
-
 }
